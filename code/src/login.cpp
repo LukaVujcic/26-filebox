f3173ccd979e81e9e502a3710e39ae3381e0b71f
mi17167 <philip.filipovic88@gmail.com>
2020-11-20 23:30:13 +0100

    feat: Implement Login form

---
+++ code/src/login.cpp
@@ -0,0 +1,22 @@
+#include "headers/login.h"
+#include "ui_login.h"
+
+Login::Login(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Login)
+{
+    ui->setupUi(this);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::paintEvent(QPaintEvent*){
+    QStyleOption opt;
+    opt.init(this);
+    QPainter p(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
+
